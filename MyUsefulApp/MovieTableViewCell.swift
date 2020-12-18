f2b05a2f71ab54f76ec73697885b621ca2f06b82
NPashaO <33179469+NPashaO@users.noreply.github.com>
2020-12-18 19:02:51 +0300

    Add files via upload

---
+++ MyUsefulApp/MovieTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MovieTableViewCell.swift
+//  MyUsefulApp
+//
+//  Created by Pasha Naumenko on 18.12.2020.
+//
+
+import Foundation
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    @IBOutlet weak var moviePosterView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieYearLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    func addMovie(movie: Movie) {
+        if movie.poster != nil {
+            moviePosterView.image = movie.poster
+        }
+        movieTitleLabel.text = movie.title
+        movieYearLabel.text = movie.year
+        typeLabel.text = movie.type
+    }
+}
